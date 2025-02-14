9c67a679eb9d6455b6e819f28a368aedbfee2cf6
aleferpar <aleferpar2@alu.edu.gva.es>
2025-02-14 16:56:29 +0100

    revision final

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import destinos from '../data/Destinos.js';
 
 const Formulario = () => {
-  const [formData, setFormData] = useState({ nombre: '', apellido1: '', apellido2: '', dni: '', telefono: '', destino1: '', destino2: '', destino3: '' });
+  const [formData, setFormData] = useState({ nombre: '', apellido1: '', apellido2: '', dni: '', telefono: '', destino1: '', destino2: '', destino3: ''});
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
   };
 
   const handleSubmit = (e) => {
@@ -18,34 +22,37 @@ const Formulario = () => {
   };
 
   const handleReset = () => {
-    setFormData({ nombre: '', apellido1: '', apellido2: '', dni: '', telefono: '', destino1: '', destino2: '', destino3: '' });
+    setFormData({ nombre: '', apellido1: '', apellido2: '', dni: '', telefono: '', destino1: '', destino2: '', destino3: ''});
   };
+
+  const { nombre, apellido1, apellido2, dni, telefono, destino1, destino2, destino3 } = formData;
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="nombre" placeholder="Nombre" value={formData.nombre} onChange={handleChange} required />
-      <input type="text" name="apellido1" placeholder="Primer Apellido" value={formData.apellido1} onChange={handleChange} required />
-      <input type="text" name="apellido2" placeholder="Segundo Apellido" value={formData.apellido2} onChange={handleChange} required />
-      <input type="text" name="dni" placeholder="DNI" value={formData.dni} onChange={handleChange} required />
-      <input type="tel" name="telefono" placeholder="Teléfono" value={formData.telefono} onChange={handleChange} required pattern="[0-9]+" />
+      <input type="text" name="nombre" placeholder="Nombre" value={nombre} onChange={handleChange} required />
+      <input type="text" name="apellido1" placeholder="Primer Apellido" value={apellido1} onChange={handleChange} required />
+      <input type="text" name="apellido2" placeholder="Segundo Apellido" value={apellido2} onChange={handleChange} required />
+      <input type="text" name="dni" placeholder="DNI" value={dni} onChange={handleChange} required />
+      <input type="number" name="telefono" placeholder="Teléfono" value={telefono} onChange={handleChange} required />
       
-      <select name="destino1" value={formData.destino1} onChange={handleChange} required>
+      <select name="destino1" value={destino1} onChange={handleChange} required>
         <option value="">Seleccione el primer destino</option>
         {destinos.map((destino, index) => (
           <option key={index} value={destino.nombre}>{destino.nombre}</option>
         ))}
       </select>
       
-      <select name="destino2" value={formData.destino2} onChange={handleChange} required>
+      <select name="destino2" disabled={!destino1 }value={destino2} onChange={handleChange} required>
         <option value="">Seleccione el segundo destino</option>
-        {destinos.map((destino, index) => (
+        
+        {destinos.filter((destino) => destino.nombre !== destino1).map((destino, index) => (
           <option key={index} value={destino.nombre}>{destino.nombre}</option>
         ))}
       </select>
       
-      <select name="destino3" value={formData.destino3} onChange={handleChange} required>
+      <select name="destino3" disabled={!destino2} value={destino3} onChange={handleChange} required>
         <option value="">Seleccione el tercer destino</option>
-        {destinos.map((destino, index) => (
+        {destinos.filter((destino) => destino.nombre !== destino1  && destino.nombre !== destino2).map((destino, index) => (
           <option key={index} value={destino.nombre}>{destino.nombre}</option>
         ))}
       </select>
--- src/data/Destinos.js
+++ src/data/Destinos.js
@@ -1,9 +1,9 @@
 const destinos = [
-    { nombre: "París", descripcion: "La ciudad del amor", foto: "paris", precio: 500 },
-    { nombre: "Londres", descripcion: "La capital británica", foto: "londres", precio: 600 },
-    { nombre: "Roma", descripcion: "Historia y cultura", foto: "roma", precio: 550 },
-    { nombre: "Nueva York", descripcion: "La gran manzana", foto: "nueva york", precio: 700 },
-    { nombre: "Tokio", descripcion: "Fusión de tradición y tecnología", foto: "tokio", precio: 750 }
+    { nombre: "París", descripcion: "La ciudad del amor", foto: "paris.jpg", precio: 500 },
+    { nombre: "Londres", descripcion: "La capital británica", foto: "londres.jpg", precio: 600 },
+    { nombre: "Roma", descripcion: "Historia y cultura", foto: "roma.jpg", precio: 550 },
+    { nombre: "Nueva York", descripcion: "La gran manzana", foto: "nueva_york.jpg", precio: 700 },
+    { nombre: "Tokio", descripcion: "Fusión de tradición y tecnología", foto: "tokio.jpg", precio: 750 }
   ];
   
   export default destinos;
